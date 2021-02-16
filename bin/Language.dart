da8bc7d7db062ace2b070cbe42128ea5f6972a25
squ1dd13 <squ1dd13dev@gmail.com>
2021-02-16 15:06:06 +0000

    Take file name from arguments

--- bin/Language.dart
+++ bin/Language.dart
@@ -7,9 +7,7 @@ import 'runtime/standard/IO.dart';
 void main(List<String> arguments) {
   registerIO();
 
-  var toLex =
-      File('/home/squ1dd13/Documents/Projects/Dart/Language/test_code/code.idk')
-          .readAsStringSync();
+  var toLex = File(arguments[0]).readAsStringSync();
 
   var lexer = Lexer(toLex);
   print(lexer.generatedTokens);
@@ -18,23 +16,4 @@ void main(List<String> arguments) {
   for (var statement in statements) {
     statement.execute();
   }
-
-  print('Done');
-
-  // try {
-  //   var stream = TokenStream(lexer.generatedTokens, 0);
-  //   var declaration = VariableDeclaration(stream);
-  //   declaration.createStatement().execute();
-  //   print(declaration);
-  //
-  //   declaration = VariableDeclaration(stream);
-  //   declaration.createStatement().execute();
-  //
-  //   var theVar = Store.current().getAs<Variable>('y');
-  //
-  //   var func = FunctionDeclaration(stream);
-  //   print(func);
-  // } on InvalidSyntaxException catch (exception) {
-  //   print('Invalid syntax: $exception');
-  // }
 }
